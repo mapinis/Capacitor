2fb13a454a3dbcb7e1a8424e952d3f217ab0a75c
Mark Apinis <mapinis2001@gmail.com>
2020-07-27 00:30:06 -0400

    Admin Button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,40 @@
 import React from "react";
 
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
-export default (props) => (
-  <div className='Header'>
-    <div className='HeaderContent'>
-      <Link to='/'>{props.title}</Link>
-    </div>
-  </div>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+    };
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect to={this.state.redirect}>
+          {this.setState({ redirect: false })}
+        </Redirect>
+      );
+    }
+
+    return (
+      <div className='Header'>
+        <div className='HeaderContent'>
+          <Link to='/'>{this.props.title}</Link>
+          <Button
+            variant='outline-light'
+            onClick={() => this.setState({ redirect: "/admin" })}
+          >
+            Admin Login
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
