b9f2f23a0c64398eb034b740e2bf97a2e4da8130
Mark Apinis <mapinis2001@gmail.com>
2020-07-27 00:44:09 -0400

    Added Nishuane to example data and changed rendering in case of large number of locations

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,19 +1,29 @@
 import React from "react";
 import LocationsContext from "../util/LocationsContext";
-import { CardDeck } from "react-bootstrap";
+import { CardDeck, CardColumns } from "react-bootstrap";
 import LocationCard from "./LocationCard";
 
 class MainPage extends React.Component {
   static contextType = LocationsContext;
 
   render() {
-    return (
-      <CardDeck className='locationCardDeck'>
-        {Object.keys(this.context).map((locationID) => (
-          <LocationCard key={locationID} locationID={locationID} />
-        ))}
-      </CardDeck>
-    );
+    if (Object.keys(this.context).length > 3) {
+      return (
+        <CardColumns className='locationCardDeck'>
+          {Object.keys(this.context).map((locationID) => (
+            <LocationCard key={locationID} locationID={locationID} />
+          ))}
+        </CardColumns>
+      );
+    } else {
+      return (
+        <CardDeck className='locationCardDeck'>
+          {Object.keys(this.context).map((locationID) => (
+            <LocationCard key={locationID} locationID={locationID} />
+          ))}
+        </CardDeck>
+      );
+    }
   }
 }
 
