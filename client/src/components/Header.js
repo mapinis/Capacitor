61b8f585bf66a6f35864c7936c60877be5aba85e
Mark Apinis <mapinis2001@gmail.com>
2020-07-31 23:42:26 -0400

    Better Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,40 +1,22 @@
 import React from "react";
 
 import "./Header.css";
-import { Link, Redirect } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      redirect: false,
-    };
-  }
+const Header = (props) => {
+  const history = useHistory();
 
-  render() {
-    if (this.state.redirect) {
-      return (
-        <Redirect to={this.state.redirect}>
-          {this.setState({ redirect: false })}
-        </Redirect>
-      );
-    }
-
-    return (
-      <div className='Header'>
-        <div className='HeaderContent'>
-          <Link to='/'>{this.props.title}</Link>
-          <Button
-            variant='outline-light'
-            onClick={() => this.setState({ redirect: "/admin" })}
-          >
-            Admin Login
-          </Button>
-        </div>
+  return (
+    <div className='Header'>
+      <div className='HeaderContent'>
+        <Link to='/'>{props.title}</Link>
+        <Button variant='outline-light' onClick={() => history.push("/admin")}>
+          Admin Login
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Header;
